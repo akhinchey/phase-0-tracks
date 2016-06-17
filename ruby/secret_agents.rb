ce129a87e327989cc9a1f49195c94067a2779064
Amanda Hinchey <akhinchey@gmail.com>
2016-06-17 14:44:07 -0400

    Added psuedocode for encrypt

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,14 @@
+# ecrypt - 
+# while there are characters in string
+#    if character advancing forward is a z
+#      move forward to single a
+#    elsif character is a space
+#      just return space
+#    else
+#      advance first letter of string one letter forward
+#    end
+#     increment index to the next letter of the string
+#   end while
+        
+def encrypt (str) 
+end
