a8bd5a318a13f8666de4285bc4a1897ed43c59ae
Amanda Hinchey <akhinchey@gmail.com>
2016-06-17 21:06:52 -0400

    Updated through Release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,37 +13,34 @@ garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
 
-i = "0"
+i = 0
 
 if age.to_i == (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" || insurance == "yes")
-  i++
+  i = 1
   
-elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" || insurance == "no")
-  i++
+elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "yes")
+  i = 2
   
-elsif age.to_i != (Time.now.year - birth_year.to_i) && garlic_bread == "no" && insurance == "no"
-  i++
+elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" && insurance == "no")
+  i = 2
   
-elsif name == "Drake Cula" || name == "Tu Fang"
-  puts "vampire"
-else
-  puts "Results inconclusive."
+elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "no")
+  i = 3
 end
 
-puts i
-
-#if age.to_i == (Time.now.year - birth_year.to_i)
-#  i++
-
-# Match the following conditions in the order they're listed. 
-# Your program should base its result on the latest condition matched, 
-# not the first condition matched. 
-# In other words, it's not a "only one condition will apply" sort of scenario. 
-# This may mean repeatedly updating a variable as each condition is checked, 
-# so that the variable always contains the most precise result.
-
-
-    
+if name === "Drake Cula" || name == "Tu Fang"
+  puts "Definitely a vampire"
+else
+  if i == 1
+    puts "Probably not a vampire"
+    elsif i == 2
+    puts "Probably is a vampire"
+    elsif i == 3
+    puts "Almost certainly a vampire"
+    elsif i == 0
+    puts "results inconclusive"
+  end
+end
 
 
 
