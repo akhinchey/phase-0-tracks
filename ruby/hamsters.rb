995156d513c4a3f32ab1f889f7e5c5abd09a44e9
Amanda Hinchey <akhinchey@gmail.com>
2016-06-18 13:01:12 -0400

    Updated program with hamster input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,10 +2,16 @@ puts "hamster's name:"
 hamster_name = gets.chomp
 
 puts "volume level from 1 to 10:"
-vol_level = gets.chomp
+vol_level = gets.chomp.to_i
 
 puts "whether the hamster is a good candidate for adoption:"
 good_candidate = gets.chomp
 
 puts "estimated age:"
-age = gets.chomp
+age = gets.chomp.to_i
+
+puts "hamster entry:" 
+puts "name: #{hamster_name}" 
+puts "volume level: #{vol_level}"
+puts "whether the hamster is a good candidate for adoption: #{good_candidate}"
+puts "estimated age: #{age}"
