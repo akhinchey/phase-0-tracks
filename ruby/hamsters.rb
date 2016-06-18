99c58aa59c9b207fd1699d72c963e6d2a0942608
Amanda Hinchey <akhinchey@gmail.com>
2016-06-18 12:51:31 -0400

    Initial commit

---
+++ ruby/hamsters.rb
@@ -0,0 +1,11 @@
+puts "hamster's name:"
+hamster_name = gets.chomp
+
+puts "volume level from 1 to 10:"
+vol_level = gets.chomp
+
+puts "whether the hamster is a good candidate for adoption:"
+good_candidate = gets.chomp
+
+puts "estimated age:"
+age = gets.chomp
