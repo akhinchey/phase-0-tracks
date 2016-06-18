dbae191f3b98fdcf2c4313ebb47d1a43e2c7e39f
Amanda Hinchey <akhinchey@gmail.com>
2016-06-17 21:15:22 -0400

    Updated through Release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,47 +1,57 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees would you like to process:"
+num_of_employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp
+j = 0
 
-puts "What year were you born?"
-birth_year = gets.chomp
+while j < num_of_employees
 
-puts "Our cafeteria serves garlic bread. Should we order you some?"
-garlic_bread = gets.chomp
+  puts "What is your name?"
+  name = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp
 
-i = 0
+  puts "What year were you born?"
+  birth_year = gets.chomp
 
-if age.to_i == (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" || insurance == "yes")
-  i = 1
-  
-elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "yes")
-  i = 2
-  
-elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" && insurance == "no")
-  i = 2
-  
-elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "no")
-  i = 3
-end
+  puts "Our cafeteria serves garlic bread. Should we order you some?"
+  garlic_bread = gets.chomp
 
-if name === "Drake Cula" || name == "Tu Fang"
-  puts "Definitely a vampire"
-else
-  if i == 1
-    puts "Probably not a vampire"
-    elsif i == 2
-    puts "Probably is a vampire"
-    elsif i == 3
-    puts "Almost certainly a vampire"
-    elsif i == 0
-    puts "results inconclusive"
+  puts "Would you like to enroll in the company's health insurance?"
+  insurance = gets.chomp
+
+  i = 0
+
+  if age.to_i == (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" || insurance == "yes")
+    i = 1
+    
+  elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "yes")
+    i = 2
+    
+  elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" && insurance == "no")
+    i = 2
+    
+  elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "no")
+    i = 3
   end
-end
 
+  if name === "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire"
+  else
+    if i == 1
+      puts "Probably not a vampire"
+      elsif i == 2
+      puts "Probably is a vampire"
+      elsif i == 3
+      puts "Almost certainly a vampire"
+      elsif i == 0
+      puts "results inconclusive"
+    end
+  end
+  
+  j += 1
+
+end
 
 
     
