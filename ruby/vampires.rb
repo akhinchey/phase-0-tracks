151f3b76273773a30290a1aaac52965e41b4aecc
Amanda Hinchey <akhinchey@gmail.com>
2016-06-17 22:37:06 -0400

    Updated with Release 4 and 5

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -19,39 +19,71 @@ while j < num_of_employees
 
   puts "Would you like to enroll in the company's health insurance?"
   insurance = gets.chomp
+  
+vampire_status = false  
+  
+  allergy_final = "not done"
+  allergy_list = "allergy list: \n"
 
-  i = 0
+  puts "Please list allergies one by one, hitting 'Enter' between each one. Type 'done' when you are done."
 
-  if age.to_i == (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" || insurance == "yes")
-    i = 1
-    
-  elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "yes")
-    i = 2
-    
-  elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" && insurance == "no")
-    i = 2
-    
-  elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "no")
-    i = 3
-  end
 
-  if name === "Drake Cula" || name == "Tu Fang"
-    puts "Definitely a vampire"
-  else
-    if i == 1
-      puts "Probably not a vampire"
-      elsif i == 2
+  while allergy_final == "not done"
+
+  puts "Enter allergy"
+  user_input = gets.chomp
+
+    if user_input == "sunshine"
+      vampire_status = true
       puts "Probably is a vampire"
-      elsif i == 3
-      puts "Almost certainly a vampire"
-      elsif i == 0
-      puts "results inconclusive"
+      allergy_final = "done"
+      elsif user_input == "done"
+      puts allergy_list
+      allergy_final = "done"
+    else
+      allergy_list += user_input + "\n"
     end
   end
   
+  if vampire_status == false
+    
+    i = 0
+  
+    if age.to_i == (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" || insurance == "yes")
+      i = 1
+      
+    elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "yes")
+      i = 2
+      
+    elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "yes" && insurance == "no")
+      i = 2
+      
+    elsif age.to_i != (Time.now.year - birth_year.to_i) && (garlic_bread == "no" && insurance == "no")
+      i = 3
+    end
+  
+    if name === "Drake Cula" || name == "Tu Fang"
+      puts "Definitely a vampire \n"
+    else
+      if i == 1
+        puts "Probably not a vampire"
+        elsif i == 2
+        puts "Probably is a vampire"
+        elsif i == 3
+        puts "Almost certainly a vampire"
+        elsif i == 0
+        puts "results inconclusive"
+      end
+    end
+    
+  end
+
   j += 1
 
 end
+
+puts  "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
 
 
     
