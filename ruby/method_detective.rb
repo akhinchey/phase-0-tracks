93831821818fb1d59f500ce6923fb8b8fb817615
Amanda Hinchey <akhinchey@gmail.com>
2016-06-22 14:56:02 -0400

    Added suspect methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,32 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+puts "iNvEsTiGaTiOn".swapcase
 
-# "zom".<???>
-# => “zoom”
+puts "zom".insert(1, "o")
 
-# "enhance".<???>
-# => "    enhance    "
+puts "enhance".center(11)
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+puts "Stop! You’re under arrest!".upcase
+puts "Hello world.".upcase
 
-# "the usual".<???>
-#=> "the usual suspects"
+puts "the usual".concat(" suspects")
+puts "the usual" << " suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+puts " suspects".prepend("the usual")
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+puts "The case of the disappearing last letter"[0...-1]
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+puts "The mystery of the missing first letter".delete "T"
+puts "The mystery of the missing first letter"[1..-1]
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+# It would be possible to use slice! for the disappearing first and last letter, 
+# but it would return the removed letter and not the string itself
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+puts "Elementary,    my   dear        Watson!".squeeze
+# the above only works if there are no side by side duplicate characters
+puts "Elementary,    my   dear        Watson!".gsub(/\s+/, " ")
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+puts "z".each_byte {|c| puts c }
+
+puts "How many times does the letter 'a' appear in this string?".count "a"
