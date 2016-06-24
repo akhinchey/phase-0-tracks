5f50f01365f91bb9571aadedbc58e39c16f52800
Amanda Hinchey <akhinchey@gmail.com>
2016-06-24 13:59:25 -0400

    init commit with pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+# Ask user for info
+# use gets chomp to store their response as the value in a certain key in the hash
+# have hash already set up with keys and empty values
+# puts user information out as list after all questions have been answered
+# ask user if they would like to modify answers
+# if yes then ask them to enter in the key whose value to modify
+# if no then skip
+# print final updated version of hash
