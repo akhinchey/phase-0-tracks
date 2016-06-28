54889e8f49f2362671703d0735774dcf11d8ca48
Amanda Hinchey <akhinchey@gmail.com>
2016-06-28 16:36:05 -0400

    Added pseudocode to puppy file and updated gps2.2

---
+++ ruby/puppy.rb
@@ -0,0 +1,27 @@
+# CLASS 
+
+# Puppy!!!
+# 
+# CHARACTERISTICS 
+
+# furry
+# noisy
+# 4 legs
+# wet nose
+# affecionate
+# playful
+# loving
+# tail
+# floppy ears
+
+# BEHAVIOR
+
+# run 
+# jump
+# fetch
+# have accidents
+# lick face
+# roll over
+# play dead
+# bark
+# pant
