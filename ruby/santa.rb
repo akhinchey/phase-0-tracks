eac94b3c6a99f7cdd7c60c5c8b21e514ea5f86e6
Amanda Hinchey <akhinchey@gmail.com>
2016-06-30 19:20:41 -0400

    Updated through Release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
 
-  attr_reader :age, :ethnicity
+  attr_reader :age, :ethnicity, :reindeer_ranking
   attr_accessor :gender
 
   def speak
@@ -25,7 +25,7 @@ class Santa
       "Donner", 
       "Blitzen"
     ]
-    @age = 0
+    @age = rand(0..140)
     puts "Initializing Santa instance ..."
   end
 
@@ -40,34 +40,38 @@ class Santa
 
 end
 
-santas = []
+# santas = []
 
 example_genders = ["male", "female", "bigender", "male", "female"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "Indian"]
+example_cookies = ["chocolate chip cookie", "peanut butter cookie", "sugar cookie", "ginger cookie"]
 
-i = 0
+# i = 0
+# while i < example_genders.length
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+#   i += 1
+# end
+# 
+# 
+# sam = Santa.new("male", "white")
+# sam.get_mad_at("Rudolph")
+# sam.gender = "female"
+# 
+# sam.get_mad_at("Dancer")
+# sam.celebrate_birthday
+# 
+# puts "Santa's current age is #{sam.age}"
+# puts "Santa's ethnicity is #{sam.ethnicity}"
 
-while i < example_genders.length
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-  i += 1
+
+for num in 1..100
+  santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+  puts "current age: #{santa.age}"
+  puts "current gender: #{santa.gender}"
+  santa.speak
+  santa.eat_milk_and_cookies(example_cookies.sample)
+  p "Happy Birthday to Santa who is now #{santa.celebrate_birthday}"
+  p santa.get_mad_at(santa.reindeer_ranking.sample)
 end
-
-santas << Santa.new("female", "white")
-
-puts santas[0]
-
-
-sam = Santa.new("male", "white")
-sam.get_mad_at("Rudolph")
-sam.gender = "female"
-
-
-sam.get_mad_at("Dancer")
-sam.celebrate_birthday
-
-puts "Santa's current age is #{sam.age}"
-puts "Santa's ethnicity is #{sam.ethnicity}"
-p sam
-
 
 
