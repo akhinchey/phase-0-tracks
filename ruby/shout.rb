777220ddde468abb7cd967c4403e87713a76e3af
Amanda Hinchey <akhinchey@gmail.com>
2016-07-11 02:05:57 -0400

    added yelling methods and river code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-  # some code here
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words + "!!!" + " :)"
+  end
 end
+
+puts Shout.yell_angrily("What the heck")
+
+puts Shout.yell_happily("Hello world")
