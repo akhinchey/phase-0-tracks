0f4d4ef715db1034d620c8862470fdeefe442682
Amanda Hinchey <akhinchey@gmail.com>
2016-07-11 02:33:58 -0400

    Updated user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -14,13 +14,16 @@
 
 class Cat
 
-  def initialize(name, sex, favorite_toy)
-    puts "initializing cat instance"
+  attr_reader :name, :sex, :age
+  attr_accessor :toy 
+
+  def initialize(name, sex, toy)
     @name = name
     @sex = sex
     @awake = true
     @age = 2
-    @favorite_toy = favorite_toy
+    @toy = toy
+    puts "initializing cat instance"
   end
 
   def yowl(num_of_yowls)
@@ -41,6 +44,49 @@ class Cat
     @awake = true
   end
 
-
-
 end
+
+ cats = []
+ 
+ create_cats = "yes"
+ 
+ puts "Welcome to Create-A-Cat!!!"
+
+ while create_cats == "yes"
+ 
+ puts "Enter your cat name:"
+ cat_name = gets.chomp
+ 
+ puts "enter sex:"
+ cat_sex = gets.chomp
+ 
+ puts "enter toy:"
+ cat_toy = gets.chomp
+ 
+ cats << Cat.new(cat_name, cat_sex, cat_toy)
+ 
+ 
+   puts "Would you like to add another cat? Type 'yes' or 'no':"
+   create_cats = gets.chomp
+ 
+     while create_cats != "yes" && create_cats != "no"
+       puts "That is not valid input. Please type 'yes' or 'no':"
+       create_cats = gets.chomp
+     end
+
+  if create_cats == "no"
+  cats.each do |cat|
+    puts "Name: #{cat.name}"
+    puts "Default age: #{cat.age}"
+    puts "Sex: #{cat.sex}"
+    puts "Favorite toy: #{cat.toy}"
+  end
+
+  end
+
+
+ end
+
+
+
+
