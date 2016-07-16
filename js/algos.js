3ab0e187e8401c6613065bef42565615a7bb8ef1
Amanda Hinchey <akhinchey@gmail.com>
2016-07-15 22:24:12 -0400

    Added final function and driver code for Release 2

--- js/algos.js
+++ js/algos.js
@@ -30,20 +30,45 @@ function randomWords(num) {
   var newWord ="";
   
   for (var i =0; i < num; i++) {
-    newWord += (alphabet[Math.round((Math.random() * 10))]);
+    newWord += (alphabet[Math.round(Math.random() * 25)]);
   }
   
   return newWord;
 }
 
 
+function randomWords(num) {
+  var wordArr =[];  
+  for(var j=0; j<num; j++) {  
+    
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    var newWord ="";
+    
+    for (var i =0; i < Math.ceil(Math.random()*10); i++) {
+      newWord += (alphabet[Math.round(Math.random() * 25)]);
+    }
+    
+    wordArr.push(newWord);
+  }
+  return wordArr;
+}
+
+
 
 // Driver code for longestStr
 var test = ["long phrase","longest phrase","longer phrase"];
-longestStr(test);
+console.log(longestStr(test));
 
 
 // Driver code for compareObjects
 var obj1 = {name: "Steven", age: 54};
 var obj2 = {name: "Tamir", age: 54};
-compareObjects(obj1, obj2);
+console.log(compareObjects(obj1, obj2));
+
+
+// Driver code for randomWords
+for (var i =0; i<10; i++) {
+  var randomArr = randomWords(Math.ceil(Math.random()*10));
+  console.log(randomArr);
+  console.log(longestStr(randomArr));
+}
