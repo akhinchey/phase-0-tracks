2d14553308f7ec80f4abf734d02815c0a9598ce3
Amanda Hinchey <akhinchey@gmail.com>
2016-07-25 16:08:13 -0400

    For fun, added some additional troubleshooting do deal with incorrect input

--- databases/release-8.5/mailing_list.rb
+++ databases/release-8.5/mailing_list.rb
@@ -14,79 +14,87 @@ SQL
 
 db.execute(create_table_cmd)
 
- thing = db.execute("SELECT * FROM card_list").flatten[1]
- puts thing
+# Just me playing with selecting specific info from the database
+# thing = db.execute("SELECT * FROM card_list").flatten[1]
+# puts thing
 
- puts "Welcome to your current Christmas card list:"
- 
- current_card_list = db.execute("SELECT * FROM card_list")
+puts "Welcome to your current Christmas card list:"
 
- current_card_list.each do |person|
-   puts "#{person[1]}: #{person[2]}"
- end
+current_card_list = db.execute("SELECT * FROM card_list")
+
+current_card_list.each do |person|
+  puts "#{person[1]}: #{person[2]}"
+end
 
 
- list_loop = true
- 
- while list_loop == true
- 
-   puts "Would you like to update your list: y or n?"
-   update_list = gets.chomp
- 
-   if update_list == "y"
- 
-     puts "How would you like to update:"
-     puts "Type 'add' to add a new name."
-     puts "Type 'update' to update an already existing address."
-     puts "Type 'delete' to delete a name"
- 
-     update_choice = gets.chomp
- 
-     case update_choice
- 
-     when "add"
-       puts "Enter first and last name:"
-       name = gets.chomp
-       puts "Enter street address:"
-       address = gets.chomp
+list_loop = true
 
-       db.execute("INSERT INTO card_list (name, address) VALUES (?,?)", [name, address])
- 
-       puts "You're adding #{name}, #{address}."
- 
-     when "update"
-       puts "Enter the name of the person who's address you want to update:"
-       name = gets.chomp
-       puts "Enter the new address for #{name}:"
-       address = gets.chomp
+while list_loop == true
 
-       db.execute("UPDATE card_list SET address = ? WHERE name = ?", [address, name])
+  puts "Would you like to update your list: y or n?"
+  update_list = gets.chomp
 
-       puts "Address for #{name} had been updated to #{address}"
+  if update_list == "y"
+
+    puts "How would you like to update:"
+    puts "Type 'add' to add a new name."
+    puts "Type 'update' to update an already existing address."
+    puts "Type 'delete' to delete a name"
+
+    update_choice = gets.chomp
+
+    case update_choice
+
+    when "add"
+      puts "Enter first and last name:"
+      name = gets.chomp
+      puts "Enter street address:"
+      address = gets.chomp
+
+      db.execute("INSERT INTO card_list (name, address) VALUES (?,?)", [name, address])
+
+      puts "You're adding #{name}, #{address}."
+
+    when "update"
+      puts "Enter the name of the person who's address you want to update:"
+      name = gets.chomp
+
+        if name == db.execute("SELECT * FROM card_list WHERE name = ?", [name])
+
+        db.execute("UPDATE card_list SET address = ? WHERE name = ?", [address,  name])
+        puts "Enter the new address for #{name}:"
+        address = gets.chomp
  
-     when "delete"
-       puts "Enter the name of the person you want to delete:"
-       name = gets.chomp
+        puts "Address for #{name} had been updated to #{address}"
+        else
+         puts "That name is currently not on the list."
+        end
+
+    when "delete"
+      puts "Enter the name of the person you want to delete:"
+      name = gets.chomp
+     
+      if name == db.execute("SELECT * FROM card_list WHERE name = ?", [name])
+  
+        db.execute("DELETE FROM card_list WHERE name = ?", [name])
+        puts "#{name} has been deleted"
+        
+      else
+        puts "That name is currently not on the list."
+      end
+
+    else
+      puts "That is not valid input."
       
-       db.execute("DELETE FROM card_list WHERE name = ?", [name])
+    end
 
-       puts "#{name} has been deleted"
- 
- 
-     else
-       puts "That is not valid input."
-       
-     end
- 
-   elsif update_list == "n"
-     puts "Okay, you're all set!"
-     list_loop = false
-   else
-     puts "That is not a valid input."
-   end
- end
-
-
+  elsif update_list == "n"
+    puts "Okay, you're all set!"
+    list_loop = false
+  else
+    puts "That is not a valid input."
+  end
+end
 
 current_card_list = db.execute("SELECT * FROM card_list")
 
